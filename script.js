0effa332a1591a773d182e9835c2f93cc6ebccf2
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 23:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //your JS code here. If required.
 let p = document.querySelector("#status");
 let button = document.querySelector("#enterBtn");
-let div=document.querySelector("#container")
+
 let body = document.querySelector("body");
 
 button.addEventListener("click", () => {
    
-	div.innerHTML="<h1 id='status'>Entered Metaverse</h1> "
+	body.innerHTML="<h1 id='status'>Entered Metaverse</h1> <br> <button id='enterBtn'>Enter</button>"
 });
-cy.get('#status').should('contain', 'Entered Metaverse');
-cy.get('body').should('have.descendants', 'h1');
+

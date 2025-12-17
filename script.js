ca40756635f6a01b62bd6e3205e7c95aa514af14
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 22:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let p=document.querySelector("#status");
 let button=document.querySelector("#enterBtn");
 let body=document.querySelector("body");
 button.addEventListener("click",()=>{
+	p.remove();
 	let h1=document.createElement("h1");
 	h1.innerText="Entered Metaverse";
 	h1.setAttribute("id","status")
 	body.prepend(h1);
 	p.remove();
-	
-})
+});

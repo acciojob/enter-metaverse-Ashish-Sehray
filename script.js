33ce0bd7fa5af0125731dba35884154009880c0b
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 23:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,3 +8,5 @@ button.addEventListener("click", () => {
    
 	div.innerHTML="<h1 id='status'>Entered Metaverse</h1> "
 });
+cy.get('#status').should('contain', 'Entered Metaverse');
+cy.get('body').should('have.descendants', 'h1');

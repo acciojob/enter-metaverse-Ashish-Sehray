1426c53fd4092197a927e3da7ebc5d95ab8471b0
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 22:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-let p=document.querySelector("#status");
-let button=document.querySelector("#enterBtn");
-let body=document.querySelector("body");
-button.addEventListener("click",()=>{
-	p.remove();
-	let h1=document.createElement("h1");
-	h1.innerText="Entered Metaverse";
-	h1.setAttribute("id","status")
-	body.prepend(h1);
-	
+let p = document.querySelector("#status");
+let button = document.querySelector("#enterBtn");
+let body = document.querySelector("body");
+
+button.addEventListener("click", () => {
+    let h1 = document.createElement("h1");
+    h1.innerText = "Entered Metaverse";
+    h1.setAttribute("id", "enteredStatus"); 
+    body.prepend(h1);
+    p.remove();
 });
